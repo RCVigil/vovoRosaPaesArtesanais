0e0148bce828093b0a939ca8b4be46f630da9261
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2025-08-01 00:40:08 -0300

    Iniciando Header

--- vovo_rosa_landing_page/src/App.tsx
+++ vovo_rosa_landing_page/src/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import vovoRosaLogo from "./assets/images/VovóRosaLogoFTransparente_semTelefone.svg";
 import "./App.css";
+import MyLandingPage from "./components/MyLandingPage";
 
 function App() {
   return (
     <>
-      <div>
-        <img src={vovoRosaLogo} className="logo" alt="Vovó Rosa logo" />
-      </div>
-      <h1>Estou Aqui</h1>
+    <MyLandingPage/>
     </>
   );
 }
---
+++ vovo_rosa_landing_page/src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <div id='Header'>
+      <a href="#Products">Pães Doces</a>
+      <a href="#Cokies">Bolachas</a>
+      <a href="#FNaturais">Pães de Fermentação Natural</a>
+      <a href="#Experiency">Experiências</a>
+      <a href="#Pedidos">Faça seu Pedido</a>
+    </div>
+  )
+}
+
+export default Header
