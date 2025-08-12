7fbcd99d185c7fb9e5aa1c30f94ae906ea36c589
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2025-08-11 23:59:37 -0300

    Atualizando Html e FavIcon

--- vovo_rosa_landing_page/src/components/Header/Header.tsx
+++ vovo_rosa_landing_page/src/components/Header/Header.tsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import './Header.scss';
+import React from "react";
+import "./Header.scss";
+import vovoRosaLogo from "../../assets/images/VovóRosaLogoFTransparente_semTelefone.svg";
 
 const Header = () => {
   return (
-    <div id='Header'>
-      <a href="#Products">Pães Doces</a>
-      <a href="#Cokies">Bolachas</a>
-      <a href="#FNaturais">Pães de Fermentação Natural</a>
-      <a href="#Experiency">Experiências</a>
-      <a href="#Pedidos">Faça seu Pedido</a>
+    <div id="Header">
+      <div className="headerLeft">
+        <a href="#sweetBreads">Pães Doces</a>
+        <a href="#Cokies">Bolachas</a>
+        <a href="#naturalFermentation">Pães de Fermentação Natural</a>
+      </div>
+      <div className="headerMeddle">
+        <img
+          src={vovoRosaLogo}
+          alt="Imagem do Logo da Vovó Rosa Pães Artesanais"
+          id="imgTopHeader"
+          width="100"
+          height="100"
+        />
+      </div>
+      <div className="headerRight">
+        <a href="#Experiency">Experiências</a>
+        <a href="#demanded">Faça seu Pedido</a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
